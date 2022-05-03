154091c56a8103c3a55ddd78e335a916d36e67db
rpc <rpc7863@gmail.com>
2022-05-03 13:32:34 +0530

    form.py changes

--- core/forms.py
+++ core/forms.py
@@ -25,14 +25,8 @@ class SignUpForm(UserCreationForm):
         }
         
         widgets = {
-    
-<<<<<<< HEAD
-            'username': forms.TextInput(attrs={'autofocus': False, 'class': 'form-control','id':'username'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control','id':'firstname'}),
-=======
             'username': forms.TextInput(attrs={'class': 'form-control','id':'username'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control','id':'username'}),
->>>>>>> 52f75341bd7bdb4462fe4549400829bc8fc3aed7
+            'first_name': forms.TextInput(attrs={'class': 'form-control','id':'firstname'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control','id':'lastname'}),
             'email': forms.TextInput(attrs={'class': 'form-control','id':'email'})
         }
